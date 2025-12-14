f6db22761d0b6857df0b27400847720ef6ff5c34
vagabond001 <roddy.ndego@acity.edu.gh>
2025-12-14 16:31:29 +0000

    update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const { Pool } = require("pg")
 require('dotenv').config()
 
+<<<<<<< Updated upstream
 const connectionString = process.env.DATABASE_URL || "postgresql://roddy:2PaXKOuM8YDge96y5FI97QyYqblyt43W@dpg-d4jqj63uibrs73f1u0f0-a.oregon-postgres.render.com/student_db_o49n"
 
 const poolConfig = { connectionString }
@@ -9,6 +10,16 @@ if (process.env.PGSSLMODE === 'require') {
 }
 
 const pool = new Pool(poolConfig)
+=======
+const pool = new Pool({
+  //updated connection string
+    connectionString: "postgresql://roddy:2PaXKOuM8YDge96y5FI97QyYqblyt43W@dpg-d4jqj63uibrs73f1u0f0-a/student_db_o49n",
+    ssl: {
+        require: true,
+        rejectUnauthorized: false,   
+    }
+})
+>>>>>>> Stashed changes
 
 pool.on("connect", () => {
     console.log("Connected to Postgres Database")
